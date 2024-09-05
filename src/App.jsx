cffc2faa48a7e31a9a106bb033b9abe3fdc09ed8
Mashur-Kabir <mashrurkabirriyan@gmail.com>
2024-09-05 18:07:45 +0600

    basic tailwind with react plugin.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h2 className="text-4xl text-center mt-9 mb-8 font-bold">Course Registration</h2>
+    </div>
+  )
+}
+
+export default App
